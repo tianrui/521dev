1b29f9cd708866cd4ff503f1700e5d9203bc5a2a
tianrui <tianrui.xiao.ray@gmail.com>
2017-07-03 17:21:33 -0400

    part 1 fc

--- A4_NN/mnist_fc.py
+++ A4_NN/mnist_fc.py
@@ -31,6 +31,8 @@ import sys
 from progressbar import ETA, Bar, Percentage, ProgressBar
 import time
 import pdb
+import numpy as np
+import matplotlib.pyplot as plt
 
 from tensorflow.examples.tutorials.mnist import input_data
 
@@ -38,21 +40,21 @@ import tensorflow as tf
 
 FLAGS = None
 
-def fc_layer(in_tensor, hidden_size):
-    W = weight_variable([in_tensor.get_shape().as_list()[1], hidden_size])
-    b = weight_variable([hidden_size])
+def fc_layer(in_tensor, hidden_size, decay_coef):
+    W = weight_variable([in_tensor.get_shape().as_list()[1], hidden_size], decay_coef)
+    b = weight_variable([hidden_size], decay_coef)
     return tf.matmul(in_tensor, W) + b
 
-def buildnet(x):
+def buildfc(x, decay_coef):
     keep_prob = tf.placeholder(tf.float32)
 
     x_flat = tf.reshape(x, [-1, 28 * 28])
-    h1 = tf.nn.relu(fc_layer(x, 512))
+    h1 = tf.nn.relu(fc_layer(x_flat, 512, decay_coef))
     h1_drop = tf.nn.dropout(h1, keep_prob)
 
-    h2 = tf.nn.relu(fc_layer(h1_drop, 128))
-    W_3 = weight_variable([128, 10])
-    b_3 = weight_variable([10])
+    h2 = tf.nn.relu(fc_layer(h1_drop, 128, decay_coef))
+    W_3 = weight_variable([128, 10], decay_coef)
+    b_3 = weight_variable([10], decay_coef)
     y = tf.matmul(h2, W_3) + b_3
 
     return y, keep_prob
@@ -124,10 +126,14 @@ def max_pool_2x2(x):
                         strides=[1, 2, 2, 1], padding='SAME')
 
 
-def weight_variable(shape):
+def weight_variable(shape, decay_coef):
   """weight_variable generates a weight variable of a given shape."""
   initial = tf.truncated_normal(shape, stddev=0.1)
-  return tf.Variable(initial)
+  var = tf.Variable(initial)
+  if decay_coef is not None:
+      decay = tf.multiply(tf.nn.l2_loss(var), decay_coef, name='weight_loss')
+      tf.add_to_collection('losses', decay)
+  return var
 
 
 def bias_variable(shape):
@@ -141,7 +147,7 @@ def part1(_):
     mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
     x = tf.placeholder(tf.float32, [None, 784])
     y_ = tf.placeholder(tf.float32, [None, 10])
-    y_fc, keep_prob = buildnet(x)
+    y_fc, keep_prob = buildnet(x, 3e-4)
     cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_fc))
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
     correct_prediction = tf.equal(tf.argmax(y_fc, 1), tf.argmax(y_, 1))
@@ -179,28 +185,49 @@ def part1(_):
         end = time.time()
         print('Time elapsed: %f' % (end-start))
 
-
-
-def main(_):
+def notmnist_fc(_):
   # Define hyperparameters
-  batch_size = 50
+  batch_size = 32
+  epochs = 25
+  train_size = 15000
+  valid_size = 1000
+  decay_coef = 3e-4
+  # Define logging arrays
+  train_ce = np.zeros(epochs)
+  train_acc = np.zeros(epochs)
+  valid_ce = np.zeros(epochs)
+  valid_acc = np.zeros(epochs)
 
   # Import data
-  mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+  data = np.load('./notMNIST.npz', 'r')
+  notmnist_x, notmnist_y = data["images"], data["labels"]
+  randIdx = np.arange(len(notmnist_x))
+  np.random.seed(521)
+  np.random.shuffle(randIdx)
+  notmnist_x = notmnist_x[randIdx]/255.
+  notmnist_y = notmnist_y[randIdx]
+  notmnist_x = np.reshape(notmnist_x, (-1, 28, 28))
+  notmnist_y = np.eye(10)[notmnist_y]
+  notmnist_xtrain, notmnist_ytrain = notmnist_x[:train_size], notmnist_y[:train_size]
+  notmnist_xvalid, notmnist_yvalid = notmnist_x[train_size:train_size+valid_size], notmnist_y[train_size:train_size+valid_size]
+  notmnist_xtest, notmnist_ytest = notmnist_x[train_size+valid_size:], notmnist_y[train_size+valid_size:]
 
   # Create the model
-  x = tf.placeholder(tf.float32, [None, 784])
+  x = tf.placeholder(tf.float32, [None, 28, 28])
 
   # Define loss and optimizer
   y_ = tf.placeholder(tf.float32, [None, 10])
 
-  # Build the graph for the deep net
-  y_conv, keep_prob = deepnn(x)
+  # Build the graph for the fc net
+  y_pred, keep_prob = buildfc(x, decay_coef)
 
   cross_entropy = tf.reduce_mean(
-      tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
-  train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-  correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+      tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_pred))
+  tf.add_to_collection('losses', cross_entropy)
+  loss = tf.add_n(tf.get_collection('losses'), name='total_loss')
+
+  train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
+  correct_prediction = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y_, 1))
   accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
   # Config session for memory
   config = tf.ConfigProto()
@@ -212,28 +239,50 @@ def main(_):
     sess.run(tf.global_variables_initializer())
     start = time.time()
 
-    for i in range(10000):
-      batch = mnist.train.next_batch(50)
-      if i % 100 == 0:
-        train_accuracy = accuracy.eval(feed_dict={
-            x: batch[0], y_: batch[1], keep_prob: 1.0})
-        print('step %d, training accuracy %g' % (i, train_accuracy))
-      train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+    for j in range(epochs):
+      for i in range((int)(train_size/batch_size)):
+          batch = [notmnist_xtrain[(i*batch_size)%len(notmnist_xtrain): ((i+1)*batch_size)%len(notmnist_xtrain)], notmnist_ytrain[(i*batch_size)%len(notmnist_ytrain): (i+1)*batch_size]]
+          if i % 100 == 0:
+            train_accuracy = accuracy.eval(feed_dict={
+                x: batch[0], y_: batch[1], keep_prob: 1.0})
+            print('step %d, training accuracy %g' % (i, train_accuracy))
+            train_ce[j] = cross_entropy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+            train_acc[j] = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+          train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+      acc = 0
+      ce = 0
+      for i in range(int(notmnist_xvalid.shape[0] / batch_size)):
+        xtest, ytest = [notmnist_xvalid[(i*batch_size)%len(notmnist_xvalid): ((i+1)*batch_size)%len(notmnist_xvalid)], notmnist_yvalid[(i*batch_size)%len(notmnist_yvalid): ((i+1)*batch_size)%len(notmnist_yvalid)]]
+        acc += accuracy.eval(feed_dict={x: xtest, y_: ytest, keep_prob: 1.0})
+        ce += cross_entropy.eval(feed_dict={x: xtest, y_: ytest, keep_prob: 1.0})
 
-      if i % 1000 == 0:
-        acc = 0
-        for i in range(int(mnist.test._num_examples / batch_size)):
-          xtest, ytest = mnist.test.next_batch(batch_size)
-          acc += accuracy.eval(feed_dict={x: xtest, y_: ytest, keep_prob: 1.0})
+      acc /= int(notmnist_xvalid.shape[0] / batch_size)
+      ce /= int(notmnist_xvalid.shape[0] / batch_size)
 
-        acc /= int(mnist.test._num_examples / batch_size)
-        print('test accuracy %g' % acc)
+      print('validation accuracy %g' % acc)
+      print('validation cross-entropy %g' % ce)
+      valid_ce[j] = ce
+      valid_acc[j] = acc
+
 
     #print('test accuracy %g' % accuracy.eval(feed_dict={
     #    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
     end = time.time()
     print('Time elapsed: %f' % (end-start))
+
+    # plot figures
+    plt.plot(train_acc, label='training accuracy')
+    plt.plot(valid_acc, label='validation accuracy')
+    plt.xlabel('epoch')
+    plt.legend()
+    plt.show()
+    plt.savefig('./part1_acc.jpg')
+    plt.plot(train_ce, label='training CE')
+    plt.plot(valid_ce, label='validation CE')
+    plt.legend()
+    plt.show()
+    plt.savefig('./part1_ce.jpg')
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
@@ -242,4 +291,5 @@ if __name__ == '__main__':
                       help='Directory for storing input data')
   FLAGS, unparsed = parser.parse_known_args()
   #tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
-  tf.app.run(main=part1, argv=[sys.argv[0]] + unparsed)
+  #tf.app.run(main=part1, argv=[sys.argv[0]] + unparsed)
+  tf.app.run(main=notmnist_fc, argv=[sys.argv[0]] + unparsed)
